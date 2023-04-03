05aa8ee45a9e60a15e876203c720dbde1231ff41
Krati <pr955213@gmail.com>
2023-04-03 10:39:46 +0530

    new code push

--- src/app/SharedComponent/shared-component/side-bar/side-bar.component.ts
+++ src/app/SharedComponent/shared-component/side-bar/side-bar.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from 'src/app/Service/shared.service';
-
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-
   constructor(private sharedService : SharedService) { }
-
   ngOnInit(): void {
   }
-
 }
+
--- src/app/SharedComponent/shared-component/top-bar/top-bar.component.ts
+++ src/app/SharedComponent/shared-component/top-bar/top-bar.component.ts
@@ -1,21 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from 'src/app/Service/shared.service';
-
-
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
-
   constructor(private sharedService : SharedService) {}
-
   ngOnInit(): void {
   }
-
   toggleme(){
    this.sharedService.emitData();
+   console.log(this.sharedService.emitData());
 }
-
 }
